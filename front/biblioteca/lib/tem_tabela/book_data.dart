009eb11c9faaeebc96408d0c3a4041dcb1ad2fa5
Carlos Eduardo Batista Araujo <mixtrik0@gmail.com>
2025-01-13 00:44:57 -0300

    tela de emprestimo

--- front/biblioteca/lib/tem_tabela/book_data.dart
+++ front/biblioteca/lib/tem_tabela/book_data.dart
@@ -5,20 +5,39 @@ import 'book_model.dart';
 List<Book> books = List.generate(
   100,
   (index) => Book(
-    codigo:'1',
-    nome: 'Livro $index',
-    isbn: 'ISBN-$index',
-    editora: index % 2 == 0 ? 'Editora A' : 'Editora B', 
-    dataPublicacao: '01/01/20${index % 30}',   
-    exemplares: List.generate(
-      5,
-      (index) => Exemplar(nomePai: 'Livro $index', situacao: index % 2 == 0 ? 'Esprestado' : 'Disponivel' , estado: index % 2 == 0 ? 'Bom' : 'Danificado')
-    )
-  ),
+      codigo: '1',
+      nome: 'Livro $index',
+      isbn: 'ISBN-$index',
+      editora: index % 2 == 0 ? 'Editora A' : 'Editora B',
+      dataPublicacao: '01/01/20${index % 30}',
+      exemplares: List.generate(
+          5,
+          (index) => Exemplar(
+              nomePai: 'Livro $index',
+              situacao: index % 2 == 0 ? 'Emprestado' : 'Disponivel',
+              estado: index % 2 == 0 ? 'Bom' : 'Danificado'))),
 );
 
 List<Book> booksEmprestimo = [
-  Book(codigo: '1234', nome: 'Livro 1', isbn: '6314592496', editora: 'Editora A', dataPublicacao: '01/01/2002'),
-  Book(codigo: '1235', nome: 'Livro 2', isbn: '2191348416', editora: 'Editora B', dataPublicacao: '01/01/2005'),
-  Book(codigo: '1236', nome: 'Livro 3', isbn: '9779666524', editora: 'Editora C', dataPublicacao: '01/01/2010')
+  Book(
+      codigo: '1234',
+      nome: 'Livro 1',
+      isbn: '6314592496',
+      editora: 'Editora A',
+      exemplares: List.empty(),
+      dataPublicacao: '01/01/2002'),
+  Book(
+      codigo: '1235',
+      nome: 'Livro 2',
+      isbn: '2191348416',
+      editora: 'Editora B',
+      exemplares: List.empty(),
+      dataPublicacao: '01/01/2005'),
+  Book(
+      codigo: '1236',
+      nome: 'Livro 3',
+      isbn: '9779666524',
+      editora: 'Editora C',
+      exemplares: List.empty(),
+      dataPublicacao: '01/01/2010')
 ];

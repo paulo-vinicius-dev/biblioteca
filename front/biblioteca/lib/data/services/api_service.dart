cae824386f2884a24bb40a76a8e307e2a8d39eda
Kevin Machado <kevinm4ch@gmail.com>
2025-01-27 18:41:44 -0300

    excluindo arquivos desnecessários

--- front/biblioteca/lib/data/services/api_service.dart
+++ front/biblioteca/lib/data/services/api_service.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:biblioteca/utils/config.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';

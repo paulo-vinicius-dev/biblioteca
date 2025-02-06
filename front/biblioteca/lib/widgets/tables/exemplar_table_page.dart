8ac16d65672351f9be647620ab9c7b1a832406ee
Otavio Sousa <otaviosousamartins14@gmail.com>
2025-02-06 15:57:18 -0300

    Revert "add breadcrumb na tela de exemplares"
    This reverts commit f5023345c5a6d37d9377130410f86daca1b70a96.

--- front/biblioteca/lib/widgets/tables/exemplar_table_page.dart
+++ front/biblioteca/lib/widgets/tables/exemplar_table_page.dart
@@ -1,20 +1,24 @@
 import 'package:biblioteca/data/models/livro_model.dart';
-import 'package:biblioteca/widgets/navegacao/bread_crumb.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:biblioteca/data/models/exemplar_model.dart';
+import 'package:biblioteca/data/models/exemplar_model.dart'; 
 import 'package:biblioteca/data/providers/exemplares_provider.dart';
 
 class ExemplaresPage extends StatefulWidget {
+  
   final Livro book;
-  const ExemplaresPage({super.key, required this.book});
+  const ExemplaresPage({
+    super.key,
+    required this.book
+  });
 
   @override
   State<ExemplaresPage> createState() => _ExemplaresPageState();
 }
 
 class _ExemplaresPageState extends State<ExemplaresPage> {
-  bool isLoading = true;
+
+  bool isLoading = true; 
 
   @override
   void initState() {
@@ -23,8 +27,9 @@ class _ExemplaresPageState extends State<ExemplaresPage> {
   }
 
   Future<void> _loadExemplares() async {
-    final provider = Provider.of<ExemplarProvider>(context, listen: false);
-    await provider.loadExemplares();
+    final provider = Provider.of<ExemplarProvider>(context,
+        listen: false); 
+    await provider.loadExemplares(); 
     setState(() {
       isLoading = false;
     });
@@ -39,11 +44,6 @@ class _ExemplaresPageState extends State<ExemplaresPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            const BreadCrumb(
-              breadcrumb: ['Inicio', 'Livros', 'Exemplares'],
-              icon: Icons.menu_book_outlined,
-            ),
-            const SizedBox(height: 16),
             if (isLoading)
               const CircularProgressIndicator()
             else
@@ -96,105 +96,103 @@ class _ExemplaresPageState extends State<ExemplaresPage> {
                         ),
 
                         // Linhas da tabela
-                        for (int i = 0;
-                            i < exemplarProvider.exemplares.length;
-                            i++)
-                          if (exemplarProvider.exemplares[i].id ==
-                              widget.book.idDoLivro)
-                            TableRow(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                      '${exemplarProvider.exemplares[i].id}'),
+                        for (int i = 0;i < exemplarProvider.exemplares.length; i++)
+                          if(exemplarProvider.exemplares[i].id == widget.book.idDoLivro)
+                          TableRow(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                    '${exemplarProvider.exemplares[i].id}'),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child:
+                                    Text(exemplarProvider.exemplares[i].isbn),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: DropdownButton<String>(
+                                  value: exemplarProvider.exemplares[i].estado
+                                      .toString(),
+                                  onChanged: (newValue) {
+                                    setState(() {
+                                      exemplarProvider.exemplares[i] = Exemplar(
+                                        id: exemplarProvider.exemplares[i].id,
+                                        cativo: exemplarProvider
+                                            .exemplares[i].cativo,
+                                        statusCodigo: exemplarProvider
+                                            .exemplares[i].statusCodigo,
+                                        estado: int.parse(newValue!),
+                                        ativo: exemplarProvider
+                                            .exemplares[i].ativo,
+                                        idLivro: exemplarProvider
+                                            .exemplares[i].idLivro,
+                                        isbn:
+                                            exemplarProvider.exemplares[i].isbn,
+                                        titulo: exemplarProvider
+                                            .exemplares[i].titulo,
+                                        anoPublicacao: exemplarProvider
+                                            .exemplares[i].anoPublicacao,
+                                        editora: exemplarProvider
+                                            .exemplares[i].editora,
+                                        idPais: exemplarProvider
+                                            .exemplares[i].idPais,
+                                        nomePais: exemplarProvider
+                                            .exemplares[i].nomePais,
+                                        siglaPais: exemplarProvider
+                                            .exemplares[i].siglaPais,
+                                      );
+                                    });
+                                  },
+                                  items: const [
+                                    DropdownMenuItem(
+                                      value: '0',
+                                      child: Text('Selecionar'),
+                                    ),
+                                    DropdownMenuItem(
+                                      value: '1',
+                                      child: Text('Bom'),
+                                    ),
+                                    DropdownMenuItem(
+                                      value: '2',
+                                      child: Text('Danificado'),
+                                    ),
+                                  ],
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child:
-                                      Text(exemplarProvider.exemplares[i].isbn),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: DropdownButton<String>(
-                                    value: exemplarProvider.exemplares[i].estado
-                                        .toString(),
-                                    onChanged: (newValue) {
-                                      setState(() {
-                                        exemplarProvider.exemplares[i] =
-                                            Exemplar(
-                                          id: exemplarProvider.exemplares[i].id,
-                                          cativo: exemplarProvider
-                                              .exemplares[i].cativo,
-                                          statusCodigo: exemplarProvider
-                                              .exemplares[i].statusCodigo,
-                                          estado: int.parse(newValue!),
-                                          ativo: exemplarProvider
-                                              .exemplares[i].ativo,
-                                          idLivro: exemplarProvider
-                                              .exemplares[i].idLivro,
-                                          isbn: exemplarProvider
-                                              .exemplares[i].isbn,
-                                          titulo: exemplarProvider
-                                              .exemplares[i].titulo,
-                                          anoPublicacao: exemplarProvider
-                                              .exemplares[i].anoPublicacao,
-                                          editora: exemplarProvider
-                                              .exemplares[i].editora,
-                                          idPais: exemplarProvider
-                                              .exemplares[i].idPais,
-                                          nomePais: exemplarProvider
-                                              .exemplares[i].nomePais,
-                                          siglaPais: exemplarProvider
-                                              .exemplares[i].siglaPais,
-                                        );
-                                      });
-                                    },
-                                    items: const [
-                                      DropdownMenuItem(
-                                        value: '0',
-                                        child: Text('Selecionar'),
-                                      ),
-                                      DropdownMenuItem(
-                                        value: '1',
-                                        child: Text('Bom'),
-                                      ),
-                                      DropdownMenuItem(
-                                        value: '2',
-                                        child: Text('Danificado'),
-                                      ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor:
+                                        const Color.fromARGB(255, 38, 42, 79),
+                                    foregroundColor: Colors.white,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8, vertical: 5),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
+                                  child: const Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Icon(Icons.edit, color: Colors.white),
+                                      SizedBox(width: 4),
+                                      Text('Editar',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          )),
                                     ],
                                   ),
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ElevatedButton(
-                                    onPressed: () {},
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor:
-                                          const Color.fromARGB(255, 38, 42, 79),
-                                      foregroundColor: Colors.white,
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 8, vertical: 5),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(5),
-                                      ),
-                                    ),
-                                    child: const Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Icon(Icons.edit, color: Colors.white),
-                                        SizedBox(width: 4),
-                                        Text('Editar',
-                                            style: TextStyle(
-                                              fontSize: 16,
-                                              fontWeight: FontWeight.bold,
-                                            )),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
+                              ),
+                            ],
+                          ),
                       ],
                     ),
                   ],

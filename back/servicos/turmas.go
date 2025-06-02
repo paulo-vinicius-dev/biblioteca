b4dd90cfcd57ec13bb0880f9fe5bb4a777a44ae0
Andrew Dylan Viana Amorim <andrewdylanvianaamorim.2@gmail.com>
2025-06-02 08:02:11 -0300

    fazendo o cadastro de turmas

--- back/servicos/turmas.go
+++ back/servicos/turmas.go
@@ -5,6 +5,31 @@ import (
 	"biblioteca/modelos"
 )
 
+func CriarTurma(turma modelos.Turma) (modelos.Turma, bool) {
+	series := PegarTodasAsSeries()
+	turnos := PegarTodosOsTurnos()
+
+	naoAchouSerie, naoAchouTurno := true, true
+
+	for _, turno := range turnos {
+		if turno.IdTurno == turma.Turno.IdTurno {
+			naoAchouTurno = false
+		}
+	}
+
+	for _, serie := range series {
+		if serie.IdSerie == turma.Serie.IdSerie {
+			naoAchouSerie = false
+		}
+	}
+
+	if naoAchouSerie || naoAchouTurno {
+		return modelos.Turma{}, false
+	}
+
+	return banco.CriarTurma(turma)
+}
+
 func PegarTurmaPorId(id int) (modelos.Turma, bool) {
 	return banco.PegarTurmaPorId(id)
 }

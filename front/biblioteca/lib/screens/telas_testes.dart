6a5cc40b874341bff003515c003f3306c5dd2488
Thalyson <thalysonelione8@gmail.com>
2025-06-02 23:22:00 -0300

    arrumando detalhes

--- front/biblioteca/lib/screens/telas_testes.dart
+++ front/biblioteca/lib/screens/telas_testes.dart
@@ -11,18 +11,6 @@ import 'package:flutter/material.dart';
 //   }
 // }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Tela de Home"),
-    );
-  }
-}
-
-
 
 
 class Emprestimo extends StatelessWidget {
